a862a57fcd0bb3df27938c7e00f111e079124bfd
Unknown <lawrance28@gmail.com>
2018-03-30 23:18:00 +0800

    emoji can switch and show description

---
+++ Emoji Dictionary/EmojiDefinitionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EmojiDefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Kuang Yao Chang on 2018/3/29.
+//  Copyright © 2018年 Kuang Yao Chang. All rights reserved.
+//
+
+import UIKit
+
+class EmojiDefinitionViewController: UIViewController {
+    
+    @IBOutlet weak var emojiDefinitionLabel: UILabel!
+    
+    @IBOutlet weak var emojiLabel: UILabel!
+    var emoji = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        emojiLabel.text = emoji
+// ["😀","💩","🦑","🏈","🚘","🕍"]
+        if emoji == "😀" {emojiDefinitionLabel.text = "A nice smile"
+            
+        }
+        
+    }
+
+
+}
--- Emoji Dictionary/EmojiTableViewController.swift
+++ Emoji Dictionary/EmojiTableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class EmojiTableViewController: UITableViewController {
 
-    var emojis = ["😀","💩","🦑","🏈","🚘"]
+    var emojis = ["😀","💩","🦑","🏈","🚘","🕍"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +31,12 @@ class EmojiTableViewController: UITableViewController {
   
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        performSegue(withIdentifier: "ourSegue", sender: nil)
+        let emoji = emojis[indexPath.row]
+        performSegue(withIdentifier: "ourSegue", sender: emoji)
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     let emojiDefVC = segue.destination as! EmojiDefinitionViewController
+        emojiDefVC.emoji = sender as! String
     
+    }
 }
